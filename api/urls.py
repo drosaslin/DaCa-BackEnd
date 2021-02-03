009528fe239a33591d9e7dabc862ded54442d7be
drosaslin <davidrosas29@hotmail.com>
2021-02-04 00:31:41 +0800

    travel api url minor modification

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from .views import TokenView, TravelReviewView
 
 urlpatterns = [
     path('tk', TokenView.as_view()),
-    path('travel', TravelReviewView.as_view()),
+    path('travel/', TravelReviewView.as_view()),
 ]
